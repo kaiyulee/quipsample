56d8766846ae03112f83e39d0de4492156f88be2
loseyear.github.io <loseyear@gmail.com>
2015-09-09 10:09:01 +0800

    update js

--- src/dir.js
+++ src/dir.js
@@ -1,18 +1,11 @@
 var React = require('react');
+var Item = require('./item');
 var Dir = React.createClass({
     render: function() {
         return (
             <div className = "dir">
                 <ul className = "list">
-                    <li className = "item" data-id = "" data-pid = "" data-name = "">
-                        <h2>文件夹名字</h2>
-                    </li>
-                    <li className = "item" data-id = "" data-pid = "" data-name = "">
-                        <h2>文件夹名字</h2>
-                    </li>
-                    <li className = "item" data-id = "" data-pid = "" data-name = "">
-                        <h2>文件夹名字</h2>
-                    </li>
+                    return <Item />
                 </ul>
             </div>
         );
