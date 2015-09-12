6f16849f4eb6181560a172c0bfd7743e29f93f24
jixiaod <ji.xiaod@gmail.com>
2015-09-12 17:30:04 +0800

    upadte node_modules

--- im/webim_server.php
+++ im/webim_server.php
@@ -2,16 +2,8 @@
 define('DEBUG', 'on');
 define('WEBPATH', __DIR__);
 
-/**
- * /vendor/autoload.php是Composer工具生成的
- * shell: composer update
- */
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/../framework/libs/lib_config.php';
 
-/**
- * Swoole框架自动载入器初始化
- */
-Swoole\Loader::vendor_init();
 
 /**
  * 注册命名空间到自动载入器中
